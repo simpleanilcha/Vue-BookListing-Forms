dcd34d9991e1971245ea681af869b958398161d5
anil.maharjan@chuchuro.com <simpleanilcha@gmail.com>
2021-04-28 03:47:58 +0545

    update code

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -8,7 +8,7 @@
     <hr>
     <h2>Filtered Books By Ownership</h2>
     <select v-model="holding">
-      <option value="" v-for="filter in filters">{{ filter }}</option>
+      <option v-for="filter in filters">{{ filter }}</option>
     </select>
     <ul>
       <book-item v-for='book in filteredBooks' :key='book.id' :book='book'></book-item>
